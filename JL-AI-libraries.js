a3beb808c77a2f484bb39823f6a420d82aa2289c
jawn-lim <junwen89@hotmail.com>
2024-10-17 21:07:16 +0800

    scan js vul AI

---
+++ JL-AI-libraries.js
@@ -0,0 +1,24 @@
+// Importing TensorFlow.js
+const tf = require('@tensorflow/tfjs');
+
+// Importing Brain.js
+const brain = require('brain.js');
+
+// Importing ML.js
+const ML = require('ml');
+
+// Example code using TensorFlow.js (vulnerable version)
+const tensor = tf.tensor([1, 2, 3, 4]);
+tensor.print();
+
+// Example using Brain.js
+const net = new brain.NeuralNetwork();
+net.train([{ input: [0, 0], output: [0] }, { input: [1, 1], output: [1] }]);
+const output = net.run([1, 0]); // [0.987]
+
+console.log('Brain.js Output: ', output);
+
+// Example using ML.js
+const SVM = ML.SVM;
+const svm = new SVM();
+console.log('ML.js SVM instance: ', svm);
