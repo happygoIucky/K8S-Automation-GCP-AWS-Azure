7504987053eadf9484a02f8a07e082b922f9ed9d
jawn-lim <junwen89@hotmail.com>
2024-10-17 21:45:38 +0800

    scan again 2

--- JL-AI-Libraries.sh
+++ JL-AI-Libraries.sh
@@ -1,6 +1,3 @@
-python3 -m venv venv
-source venv/bin/activate
+python3 -m venv myenv
+source myenv/bin/activate
 pip install tensorflow==2.3.0 scikit-learn==0.22 torch==1.0.1
-mkdir vulnerable-ollama && cd vulnerable-ollama
-npm init -y
-npm install ollama@v0.1.11
